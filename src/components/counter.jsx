b53e5a405644b3998a8af5d025f36a600b7913c3
Jay Bettinger <jaymbett@gmail.com>
2022-09-01 20:37:54 -0600

    -fleshed out the counter interface

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-    };
+  state = {
+    count: 0,
+  };
 
-    render() { 
-        return ( 
-            <React.Fragment>
-                <span className = "badge badge-primary m-2">{this.formatCount()}</span>
-                <button className = "btn btn-secondary btn-sm">Increment</button>
-            </React.Fragment>
-        );
-    }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
 
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
-    };
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    const { count } = this.state;
+    return count === 0 ? "badge badge-warning m-2" : "badge badge-primary m-2";
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
